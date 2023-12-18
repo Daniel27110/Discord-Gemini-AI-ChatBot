69467df8178ff1e572e70272fc9e76440c627b38
Daniel Felipe Vargas <d.vargasu@uniandes.edu.co>
2023-12-18 16:15:58 -0500

    added error module & env sample

--- src/commands/chat.py
+++ src/commands/chat.py
@@ -1,13 +1,39 @@
 from discord import app_commands
 from dotenv import load_dotenv
+from errors import getErrorMessages
+import ai_chat_api.palm as palm
+import random
 import os
 
-import ai_chat_api.palm as palm
 
 # loads the .env file and token
 load_dotenv()
 
-# TODO: import personality prompt from .env file
+# import the personality prompt
+personality_prompt: str | None = os.getenv("PERSONALITY_PROMPT")
+
+# import error messages
+errors: list[str] = getErrorMessages()
+
+
+# creates the chat message in the apropiate format
+def format_message(interaction, message: str):
+    # format = "Hi I'm {username}, {personality_prompt} \n {message}"
+    username: str = interaction.user.nick or interaction.user.name
+    introduction: str = f"Hi I'm {username}, {personality_prompt}"
+    formated_message: str = f"{introduction} \n {message}"
+
+    return formated_message
+
+
+# cleans the message from the bot's response
+def clean_response(response: str) -> str:
+    # if the message has over 2000 characters, it will be cut
+    # and the last 3 characters will be replaced with "..."
+    if len(response) > 2000:
+        response = response[:1997] + "..."
+
+    return response
 
 
 # loads the chat command
@@ -15,12 +41,20 @@ def load(tree):
     @tree.command(name="chat", description="Single message chat command.")
     @app_commands.describe(message="Your message.")
     async def chat(interaction, message: str):
+        # defer the response
         await interaction.response.defer()
-        # Generate a response.
-        response = palm.ask(message)
 
-        print(response)
+        # tries to get the message
+        try:
+            # Generate a response.
+            formated_message: str = format_message(interaction, message)
+            response: str = palm.ask(formated_message)
 
-        await interaction.followup.send(response)
+            await interaction.followup.send(clean_response(response))
+
+        except Exception as e:
+            print(e)
+            # print a random error message from the errors list
+            await interaction.followup.send(random.choice(errors))
 
     print("Loaded app command: chat")
